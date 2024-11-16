a549a91b57e10ee36922b16592d842bb246d8be6
sean choi <sean.sanggyu@gmail.com>
2024-11-16 15:11:57 +0000

    modified task1

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -152,7 +152,7 @@ class ExperimentBuilder(nn.Module):
             if p.requires_grad and "bias" not in n and "batch_norm" not in n:
                 print('Layer:', n, 'Gradient:', p.grad)
                 layer_name = n.relace('layer_dict.', '_')
-                layer_name = layer_name.replace(',','')
+                layer_name = layer_name.replace('.','')
                 if layer_name.startwith('_'):
                     layer_name = layer_name[1:]
                 layers.append(layer_name.replace('weight',''))
