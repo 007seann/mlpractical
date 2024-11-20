04ce5699cd8afb32717c0c0f92cce342d41d1ec7
007seann <sean.sanggyu@gmail.com>
2024-11-20 15:01:03 +0000

    db on bn

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -155,7 +155,7 @@ class ExperimentBuilder(nn.Module):
                 if layer_name.startswith('_'):
                     layer_name = layer_name[1:]
                 layers.append(layer_name.replace('weight',''))
-                all_grads.append(p.grad.abs().mean().cpu().detach().numpy())        
+                all_grads.append(p.grad.abs().mean())       
             
                 
         plt = self.plot_func_def(all_grads, layers)
