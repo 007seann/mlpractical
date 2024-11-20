9c1be347da7b6b5f3d20092d72157eefde3450fc
007seann <sean.sanggyu@gmail.com>
2024-11-20 15:14:06 +0000

    db on bn

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -150,15 +150,12 @@ class ExperimentBuilder(nn.Module):
         """
         for n, p in named_parameters:
             if p.requires_grad and ('batch_norm' not in n) and ('bias' not in n):
-                print( "parameters",p)
-                # print('para.grade', p.grad)
-                print('name:', n)
                 layer_name = n.replace('layer_dict.', '_')
                 layer_name = layer_name.replace('.','')
                 if layer_name.startswith('_'):
                     layer_name = layer_name[1:]
                 layers.append(layer_name.replace('weight',''))
-                all_grads.append(p.grad.abs().mean())       
+                all_grads.append(p.grad.abs().mean().cpu().detach().numpy())       
             
                 
         plt = self.plot_func_def(all_grads, layers)
